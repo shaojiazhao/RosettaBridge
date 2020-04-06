88769ebb5a9e360712bbfe86dd9f9aedec56a383
Shaojia Zhao <shaojiazhao@gmail.com>
2020-04-05 19:39:03 -0700

    Initial Commit

---
+++ RosettaBridge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RosettaBridge
+//
+//  Created by Shaojia Zhao on 4/5/20.
+//  Copyright © 2020 Shaojia Zhao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
